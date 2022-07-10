0a67aa462faa9df7626cebf5f3147cc8357d074b
shakkedriver <shakked95@gamil.com>
2022-07-10 22:55:07 +0300

    car factory class was created

---
+++ CarFactory.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+class CarFactory(ABC):
+    """
+    this class is used to create cars in a certain path. each time we will call the create_car abstract method it will
+    return a new car or None
+    """
+    def __init__(self,path):
+        self.path = path
+
+    @abstractmethod
+    def create_car(self):
+        """
+        :return: a new car object or None if no car should be created
+        """
+        pass
