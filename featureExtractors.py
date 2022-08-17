84f0159052e48cc66921d893617548a156b5e808
noamCaspi <noam.caspi@mail.huji.ac.il>
2022-08-17 14:04:49 +0300

    feature extractor updated to our problem

--- featureExtractors.py
+++ featureExtractors.py
@@ -11,10 +11,14 @@
 # from game import Directions, Actions
 # import util
 from Counter import Counter
+LENGTH_OF_PATH = 150
+from NormalEnvironment import JUNCTION_SIZE
+from NormalCarFactory import NORMAL_CAR_MAX_INIT_SPEED
+import numpy as np
 
 
 class FeatureExtractor:
-    def getFeatures(self, state, action):
+    def getFeatures(self, speed_state, age_state, action, car):
         """
         Returns a dict from features to counts
         Usually, the count will just be 1.0 for
@@ -24,10 +28,11 @@ class FeatureExtractor:
 
 
 class IdentityExtractor(FeatureExtractor):
-    def getFeatures(self, state, action):
-        feats = Counter()
-        feats[(state, action)] = 1.0
-        return feats
+    pass
+    # def getFeatures(self, speed_state, age_state, action):
+    #     feats = Counter()
+    #     feats[(state, action)] = 1.0
+    #     return feats
 
 
 # def closestFood(pos, food, walls):
@@ -62,33 +67,73 @@ class SimpleExtractor(FeatureExtractor):
     - whether a ghost is one step away
     """
 
-    def getFeatures(self, state, action):
-        # extract the grid of food and wall locations and get the ghost locations
-        food = state.getFood()
-        walls = state.getWalls()
-        ghosts = state.getGhostPositions()
-
+    def getFeatures(self, speed_state, age_state, speed_change, car):
         features = Counter()
 
+        # features of me:
+        car_speed = max(0, min(car.speed + speed_change, car.max_speed))
+        features["my_speed"] = car_speed
+        my_age = car.age + 1
+        # features["my_age"] = my_age
+
+        my_cur_dist = LENGTH_OF_PATH - JUNCTION_SIZE - car.dist - 1
+        my_dist = my_cur_dist - car_speed
+        features["my_dist"] = my_dist
+
+        my_time = my_dist / car_speed
+        # features["my_time"] = my_time
+
+        # last_cars = np.where(speed_state[car.path] != -1)
+        # if len(last_cars) > 1:
+        #     last_car = car.dist - last_cars[-2]
+        # else:
+        #     last_car = car.dist
+        # features["my_last_car"] = last_car
+
+        # features of the other car:
+        other_path = 1 - car.path
+        other_last_cars = np.where(speed_state[other_path] != -1)
+        if len(other_last_cars) == 0:
+            other_speed = NORMAL_CAR_MAX_INIT_SPEED
+            other_age = 0
+            other_dist = LENGTH_OF_PATH - JUNCTION_SIZE
+            other_time = (LENGTH_OF_PATH - JUNCTION_SIZE) / NORMAL_CAR_MAX_INIT_SPEED
+
+            # features["other_speed"] = NORMAL_CAR_MAX_INIT_SPEED
+            # features["other_age"] = 0
+            # features["other_dist"] = LENGTH_OF_PATH - JUNCTION_SIZE
+            # features["other_time"] = (LENGTH_OF_PATH - JUNCTION_SIZE) / NORMAL_CAR_MAX_INIT_SPEED
+            # features["other_last_car"] = LENGTH_OF_PATH - JUNCTION_SIZE
+        else:
+            other_car = other_last_cars[-1]
+            other_speed = speed_state[other_path][other_car]
+            other_age = age_state[other_path][other_car] + 1
+
+            # features["other_speed"] = speed_state[other_path][other_car]
+            # features["other_age"] = age_state[other_path][other_car] + 1
+
+            current_other_dist = LENGTH_OF_PATH - JUNCTION_SIZE - other_car - 1
+            other_dist = current_other_dist - other_speed
+            # features["other_dist"] = other_dist
+
+            other_time = other_dist / other_speed
+            # features["other_time"] = other_time
+
+            # last_cars_other = np.where(speed_state[car.path] != -1)
+            # if len(other_last_cars) > 1:
+            #     last_car_other = other_car - last_cars_other[-2]
+            # else:
+            #     last_car_other = other_car
+            # features["other_last_car"] = last_car_other
+        if my_time < other_time:
+            first_speed = car_speed
+        else:
+            first_speed = other_speed
+        first_time = JUNCTION_SIZE // first_speed + 1
+        time_diff = min(first_time, abs(other_time - my_time))
+
+        features["time_diff"] = time_diff
         features["bias"] = 1.0
-
-        # compute the location of pacman after he takes the action
-        x, y = state.getPacmanPosition()
-        dx, dy = Actions.directionToVector(action)
-        next_x, next_y = int(x + dx), int(y + dy)
-
-        # count the number of ghosts 1-step away
-        features["#-of-ghosts-1-step-away"] = sum(
-            (next_x, next_y) in Actions.getLegalNeighbors(g, walls) for g in ghosts)
-
-        # if there is no danger of ghosts then add the food feature
-        if not features["#-of-ghosts-1-step-away"] and food[next_x][next_y]:
-            features["eats-food"] = 1.0
-
-        dist = closestFood((next_x, next_y), food, walls)
-        if dist is not None:
-            # make the distance a number less than one otherwise the update
-            # will diverge wildly
-            features["closest-food"] = float(dist) / (walls.width * walls.height)
+        features["collision"] = int(my_dist >= LENGTH_OF_PATH - JUNCTION_SIZE and other_dist >= LENGTH_OF_PATH - JUNCTION_SIZE)
         features.divideAll(10.0)
         return features
